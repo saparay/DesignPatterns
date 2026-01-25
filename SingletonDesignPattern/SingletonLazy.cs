ed9ddf8f862889a9f85488f6aef9078b4248e12f
Mani Chandra Saparay <saparay4@gmail.com>
2026-01-25 20:44:46 +0530

    thread safe way to implement

---
+++ SingletonDesignPattern/SingletonLazy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern
+{
+    public sealed class SingletonLazy
+    {
+        private static int Counter = 0;
+
+        private static readonly Lazy<SingletonLazy> instance = new Lazy<SingletonLazy>(() => new SingletonLazy());
+
+        private SingletonLazy() {
+            Counter++;
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+
+        public static SingletonLazy GetInstance()
+        {
+            return instance.Value;
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ SingletonDesignPattern/SingletonLocking.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonDesignPattern
+{
+    public sealed class SingletonLocking
+    {
+        private static int Counter = 0;
+
+        private static SingletonLocking Instance = null;
+
+        private static readonly object Instancelock = new object();
+
+        public static SingletonLocking GetInstance()
+        {
+            //locking
+            //lock (Instancelock)
+            //{
+            //    if(Instance == null)
+            //    {
+            //        Instance = new SingletonLocking();
+            //    }
+            //}
+            //return Instance;
+
+            // double-checked locking 
+
+            if(Instance == null)
+            {
+                lock (Instancelock)
+                {
+                    if (Instance == null)
+                    {
+                        Instance = new SingletonLocking();
+                    }
+                }
+            }
+            return Instance;
+        }
+
+        private SingletonLocking()
+        {
+            Console.WriteLine("Counter Value " + Counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message); 
+        }
+    }
+}
